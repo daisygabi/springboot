2a5972f7339e51462578595a318aa4cbbddf438f
Gabriela Radu <gabrielaradu.developer@gmail.com>
2017-10-14 20:53:49 +0300

    Specify which packages to scan.

--- src/main/java/com/gra/smarthome/SpringBootFunApplication.java
+++ src/main/java/com/gra/smarthome/SpringBootFunApplication.java
@@ -3,7 +3,7 @@ package com.gra.smarthome;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.gra.smarthome.controllers", "com.gra.smarthome.services"})
 public class SpringBootFunApplication {
 
 	public static void main(String[] args) {
